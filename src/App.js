49dc18510fbaec780553800bec1642340725c3f5
Adam Jones <adam@linuxtampa.com>
2019-08-29 13:37:12 -0400

    Added a file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import Cast from './components/Cast'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <Cast />
   }
 }
 
---
+++ src/components/Cast.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+
+const Cast = () => {
+  return (
+    <div>
+      <p>This is the cast page</p>
+    </div>
+  )
+}
+
+export default Cast
