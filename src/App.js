34112a63c96554e2984c058e1ab0ebff8166ba09
Adam Jones <adam@linuxtampa.com>
2019-08-31 17:58:40 -0400

    Added ul an li on the data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 import Cast from './components/Cast'
 
 class App extends Component {
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const HomePage = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  //async cannot go on components
+  const getTopMovies = async () => {
+    // const fetchData = async () => {
+    const resp = await axios.get(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=c2cccc08a093d5b7cfe606b5745c1af5&language=en-US&page=1`
+    )
+    console.log(resp.data)
+    // console.log('list of all top rated TV shows', resp.data.results)
+    // setTopMovies(resp.data)
+  }
+
+  useEffect(() => {
+    getTopMovies()
+  }, [])
+
+  return (
+    <>
+      {topMovies.map((movie, i) => {
+        return <h3 key={i}>{movie.results}</h3>
+      })}
+    </>
+  )
+}
+
+export default HomePage
